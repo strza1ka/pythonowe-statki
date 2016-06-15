e92fb6cf667bde3b70f59763a6833b50809c9982
uk4szt <lukasztyczynski20@gmail.com>
2016-06-15 12:33:21 +0200

    Wątek do dodania w kodzie
    - odmierza czas gry (tutaj czas wpisywania z klawiatury tekstu)

---
+++ watek_czas.py
@@ -0,0 +1,21 @@
+﻿#wątek liczący sekundy
+
+from threading import Thread
+from time import sleep
+sekundy=0
+minuty=0
+def Timer():
+    while True:
+        sleep(1)
+        global sekundy
+        sekundy=sekundy+1
+        
+  
+if(__name__ == '__main__'):
+    t = Thread(target=Timer,
+    args=())
+    t.start()
+    print("Wątek uruchomiony !")
+    data = input("napisz coś:")
+    print(sekundy,"sekundy")
+
