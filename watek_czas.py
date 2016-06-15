fa927e8d28796068dfb82e7a00366b469ffab7e8
uk4szt <lukasztyczynski20@gmail.com>
2016-06-15 17:19:35 +0200

    Wątek liczący czas gotowy
    żeby wyświetlić czas gry użyj metody PrintTime()

--- watek_czas.py
+++ watek_czas.py
@@ -8,14 +8,25 @@ def Timer():
     while True:
         sleep(1)
         global sekundy
+        global minuty
         sekundy=sekundy+1
-        
+        if sekundy ==60:
+            sekundy =0
+            minuty=minusty+1
+
+def PrintTime():
+    if sekundy<10:
+        sekundyString = "0" + str(sekundy)
+    print("[",minuty,":",sekundyString,"] ",sep="",end="")
   
 if(__name__ == '__main__'):
     t = Thread(target=Timer,
     args=())
     t.start()
+    PrintTime()
     print("Wątek uruchomiony !")
+    PrintTime()
     data = input("napisz coś:")
-    print(sekundy,"sekundy")
+    PrintTime()
+
 
